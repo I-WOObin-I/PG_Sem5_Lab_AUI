c4c716d52bb4ddaed2ac197b1515c054897ee4c5
I-WOObin-I <lubin07000@gmail.com>
2023-01-24 00:27:49 +0100

    lab 6 ready

--- lab_6/simple-rpg-spring-rest/src/main/java/pl/edu/pg/eti/kask/rpg/controller/FileController.java
+++ lab_6/simple-rpg-spring-rest/src/main/java/pl/edu/pg/eti/kask/rpg/controller/FileController.java
@@ -1,6 +1,7 @@
 package pl.edu.pg.eti.kask.rpg.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,8 +13,10 @@ import pl.edu.pg.eti.kask.rpg.entity.File;
 import pl.edu.pg.eti.kask.rpg.service.FileService;
 import pl.edu.pg.eti.kask.rpg.utils.FileUtils;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 @RestController
@@ -72,5 +75,29 @@ public class FileController {
                         .pathSegment("api", "files", "{id}").buildAndExpand(file.getId()).toUri())
                 .body(file.getId())).orElseGet(() -> ResponseEntity.notFound().build());
 
+    }
+
+    @GetMapping("download/{id}")
+    public ResponseEntity<byte[]> download(@PathVariable("id") Long id) throws IOException {
+        if(fileService.find(id).isPresent()) {
+            var f = fileService.find(id).get();
+            java.io.File file = new java.io.File(f.getFilePath());
+
+            Path path = Paths.get(file.getAbsolutePath());
+            byte[] resource = FileUtils.readAllBytes(String.valueOf(path));
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+            headers.add("Pragma", "no-cache");
+            headers.add("Expires", "0");
+            headers.setContentDispositionFormData("attachment", file.getName());
+            String mimeType = Files.probeContentType(path);
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentType(MediaType.parseMediaType(mimeType))
+                    .contentLength(file.length())
+                    .body(resource);
+        }
+        return ResponseEntity.notFound().build();
     }
 }

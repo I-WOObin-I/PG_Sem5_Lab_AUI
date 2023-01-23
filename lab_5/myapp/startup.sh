ff537fdcdbe53e2f7c9d069dcf49a3050caf4a3d
I-WOObin-I <lubin07000@gmail.com>
2023-01-23 01:43:24 +0100

    proxy workin

--- lab_5/myapp/startup.sh
+++ lab_5/myapp/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 java -jar /app/planetary-info-planet.jar > planetlog.log 2>&1 &
-echo planets api started
-sleep 9
+echo planets api starting
+sleep 5
 java -jar /app/planetary-info-star-system.jar > starlog.log 2>&1 &
-echo star systems api started
-sleep 9
+echo star systems api starting
+sleep 13
 java -jar /app/planetary-info-gateway.jar > gatewaylog.log 2>&1 &
-echo gateway api started
+echo gateway api starting
